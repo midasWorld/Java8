176d30b7d25b1c51ececebeec4bf9ed71b969d38
midasworld <midas117@knou.ac.kr>
2022-02-11 21:45:44 +0900

    init: 프로젝트 생성 & assertj 라이브러리 추가

---
+++ test/com/MainTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class MainTest {
+
+    @Test
+    public void 메인() {
+        boolean message = true;
+        Assertions.assertThat(message).isTrue();
+    }
+}
