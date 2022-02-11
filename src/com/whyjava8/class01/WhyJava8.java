15bdbd1d3261c730ff50c2f27eb8bd6e90b36440
midasworld <midas117@knou.ac.kr>
2022-02-11 22:33:29 +0900

    feat: 모던 자바 - 배워야 하는 이유 실습 완료

---
+++ src/com/whyjava8/class01/WhyJava8.java
@@ -0,0 +1,28 @@
+package com.whyjava8.class01;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class WhyJava8 {
+    public void whyJava() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Before
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < numbers.size(); i++) {
+            stringBuilder.append(numbers.get(i));
+            if (i != numbers.size() - 1) {
+                stringBuilder.append(" : ");
+            }
+        }
+        System.out.println("Before : " + stringBuilder);
+
+        // After
+        String result = numbers.stream()
+                .map(String::valueOf)
+                .collect(joining(" : "));
+        System.out.println("After : " + result);
+    }
+}
---
+++ test/com/whyjava8/class01/WhyJava8Test.java
@@ -0,0 +1,13 @@
+package com.whyjava8.class01;
+
+import org.junit.jupiter.api.Test;
+
+class WhyJava8Test {
+
+    WhyJava8 whyJava = new WhyJava8();
+
+    @Test
+    public void 메인테스트() {
+        whyJava.whyJava();
+    }
+}
