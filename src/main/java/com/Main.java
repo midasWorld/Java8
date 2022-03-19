b9672d1d931b9b7cb656071b76a7e98da885c2ae
midas <midas117@knou.ac.kr>
2022-03-19 22:52:19 +0900

    refactor: 프로젝트에 Maven 추가 & 패키지 이동 / Lib - commons, lombok 등 추가

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,6 +2,5 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,3 @@
-package com;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
