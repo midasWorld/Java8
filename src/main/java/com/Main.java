47a994b441f8adf30c6f8cfc504fdd835525a9a1
midas <midas117@knou.ac.kr>
2022-03-23 09:35:33 +0900

    feat: interface - Runnable 이란?

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,6 +1,18 @@
 package com;
 
-public class Main {
+// 모든 함수가 추상 메소드로 구성된 클래스!
+interface Runnable {
+    void run();
+}
+
+public class Main implements Runnable {
     public static void main(String[] args) {
+        Runnable m = new Main();
+        m.run();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello World");
     }
 }
